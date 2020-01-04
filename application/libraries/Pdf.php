e50efa21cd85882eeddfa7d8698b5e40c7104810
alittihadalislami <alittihadalislami@gmail.com>
2020-01-04 21:09:39 +0700

    Dom pdf not foung fixed

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,5 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once(dirname(__FILE__) . '/dompdf/autoload.inc.php');
+
 use Dompdf\Dompdf;
 class Pdf extends Dompdf{
 
