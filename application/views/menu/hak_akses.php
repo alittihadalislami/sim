e35394c8f3b2194a1119bb1dced7875ba0c445b0
Bismillah_123 <alittihadalislami@gmail.com>
2020-06-10 20:08:34 +0700

    Update hak_akses.php

--- application/views/menu/hak_akses.php
+++ application/views/menu/hak_akses.php
@@ -18,7 +18,7 @@
   <section class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-10">
+        <div class="col">
           <div class="card elevation-2">
             <div class="card-header bg-success">
             </div>
@@ -31,37 +31,35 @@
                   <table class="table table-striped">
                     <tbody><tr>
                       <th style="width: 10px">#</th>
-                      <th>Menu | Submenu</th>
-                      <?php foreach ($user_rule as $ur): ?>
-                        <th style="width: 80px"><?= $ur->id_rule ?></th>
-                      <?php endforeach ?>
+                      <th style="width: 30%">Menu | Submenu</th>
+                      <th>Rule</th>
                     </tr>
                     
                     <?php $no=1; foreach ($menu as $m): ?>
                       <tr>
                         <td><?= $no++ ?></td>
                         <td><?= $m->nama_menu ?></td>
-                        <?php foreach ($user_rule as $ur): ?>
                           <td>
+                        <?php foreach ($user_rule as $ur): ?>
                             <?php $akses = $this->um->isAksesMenu($ur->id_rule,$m->id_menu); ?>
                             <span class="cek-input" data-menu="<?= $m->id_menu?>" data-rule="<?=$ur->id_rule?>" data-kategori="2">
-                              <input class="cek-input" type="checkbox" <?= $akses == 1 ? 'checked' : null ?> data-toggle="toggle"  data-size="xs" data-on="<?= $ur->nama_rule ?>" data-off="<?= $ur->nama_rule ?>" data-onstyle="success" data-offstyle="secondary">
+                              <input class="cek-input" type="checkbox" <?= $akses == 1 ? 'checked' : null ?> data-toggle="toggle"  data-size="xs" data-on="<?= $ur->id_rule.'. '.$ur->nama_rule ?>" data-off="<?= $ur->nama_rule ?>" data-onstyle="success" data-offstyle="secondary">
                             </span>
-                          </td>
                         <?php endforeach ?>
+                          </td>
                       </tr>
                       <?php $urut=1; foreach ($sub_menu as $s): ?>
                         <?php if ($s->menu_id == $m->id_menu): ?>
                           <td></td>
                             <td><?=($no-1).'.'.$urut++.' &nbsp '. $s->nama_submenu ?></td>
-                            <?php foreach ($user_rule as $ur): ?>
                               <td>
+                            <?php foreach ($user_rule as $ur): ?>
                                 <?php $akses = $this->um->isAksesSubmenu($ur->id_rule,$s->id_submenu); ?>
                                 <span class="cek-input" data-menu="<?= $s->id_submenu?>" data-rule="<?=$ur->id_rule?>" data-kategori="3">
                                   <input type="checkbox"  <?= $akses == 1 ? 'checked' : null ?> data-toggle="toggle" data-size="xs" data-on="<?= $ur->nama_rule ?>" data-off="<?= $ur->nama_rule ?>" data-onstyle="success" data-offstyle="secondary">
                                 </span>
-                              </td>
                             <?php endforeach ?>
+                              </td>
                           </tr>
                         <?php endif ?>
                       <?php endforeach ?>
