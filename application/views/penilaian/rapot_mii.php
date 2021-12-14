38ed19a5da4ff961422aea5ba89c09754f0dbb1d
fu4d1-bismillah <alittihadalislami@gmail.com>
2021-12-14 12:04:53 +0700

    Update rapot_mii.php

--- application/views/penilaian/rapot_mii.php
+++ application/views/penilaian/rapot_mii.php
@@ -274,8 +274,8 @@
             <?php endif ?>
 
             <div class="tanggal" style="margin-top: 20px">
-              <div class="ar">شمبلونج، ٢٢ ذوالقعدة  ١٤٤٢</div>
-              <div class="id">Camplong, 3 Juli 2021</div>
+              <div class="ar">شمبلونج، ١٤ جمادى الأولى  ١٤٤٣</div>
+              <div class="id">Camplong, 18 Desember 2021</div>
             </div>
 
             <div class="ttd" style="text-align: center; margin-top: 20px">
