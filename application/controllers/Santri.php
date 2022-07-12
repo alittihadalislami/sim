5d388d3130347c25c6dd990977674920af677b6b
fu4d1-bismillah <alittihadalislami@gmail.com>
2022-07-13 05:42:18 +0700

    Update Santri.php

--- application/controllers/Santri.php
+++ application/controllers/Santri.php
@@ -411,7 +411,7 @@ class Santri extends CI_Controller {
 	{
 		$daput = $this->input->post(null,true);
 	
-		$tahun_id = 6; //awal tahun aktif semester 1 diubah manual
+		$tahun_id = 8; //awal tahun aktif semester 1 diubah manual
 		$template2 = [1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,];
 		$id_santri = $daput['idsantri'];
 		$id_kelas = $daput['idkelas'];
