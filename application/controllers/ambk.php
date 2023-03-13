0399935d89f9c388bb6f537d85c56cacff229509
fu4d1-bismillah <alittihadalislami@gmail.com>
2023-03-13 11:41:10 +0700

    Update ambk.php

--- application/controllers/ambk.php
+++ application/controllers/ambk.php
@@ -1,9 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Ambk extends CI_Controller {
 	public function index()
 	{
-        $this->load->view('ambk/link_ambk');
+        // $this->load->view('ambk/link_ambk');
     }
 }
