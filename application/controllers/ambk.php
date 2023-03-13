68830804cc9c85fa27f5ee7157b7f70f2ee2ff79
fu4d1-bismillah <alittihadalislami@gmail.com>
2023-03-13 10:27:39 +0700

    Update ambk.php

--- application/controllers/ambk.php
+++ application/controllers/ambk.php
@@ -2,11 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ambk extends CI_Controller {
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
 	public function index()
 	{
         $this->load->view('ambk/link_ambk');
